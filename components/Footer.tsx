821f4d9f864aea0eb7aa734b715b1ec170de9dc5
Tiago Pimenta Da Costa <44909817+TiagoPimCosta@users.noreply.github.com>
2025-05-13 11:51:17 +0100

    feat/PF-003
    * feat: Add Navbar and Footer components, integrate Tabler icons, and update layout with download button
    * feat: Add About, Portfolio, and Skills pages; update Navbar for new routes

---
+++ components/Footer.tsx
@@ -0,0 +1,37 @@
+import { IconBrandGithub, IconBrandLinkedin, IconMail } from "@tabler/icons-react";
+
+const footerItems = [
+  {
+    url: "https://www.linkedin.com/in/tiagopimcosta/",
+    icon: IconBrandLinkedin,
+  },
+  {
+    url: "https://github.com/TiagoPimCosta",
+    icon: IconBrandGithub,
+  },
+  {
+    url: "/",
+    icon: IconMail,
+  },
+];
+
+export default function Footer() {
+  return (
+    <footer className="mt-16">
+      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
+        {footerItems.map((item, index) => (
+          <li key={`footer-${index}-element`}>
+            <a
+              className="flex items-center h-7 gap-1 transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+              rel="noopener noreferrer"
+              target="_blank"
+              href={item.url}
+            >
+              <item.icon className="h-7 w-7" />
+            </a>
+          </li>
+        ))}
+      </ul>
+    </footer>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,54 @@
+"use client";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const navItems = {
+  "/": {
+    name: "home",
+  },
+  "/about": {
+    name: "about",
+  },
+  "/skills": {
+    name: "skills",
+  },
+  "/portfolio": {
+    name: "portfolio",
+  },
+  "/services": {
+    name: "services",
+  },
+};
+
+export function Navbar() {
+  const pathname = usePathname();
+
+  return (
+    <aside className="-ml-[8px] mb-16 tracking-tight">
+      <div className="lg:sticky lg:top-20">
+        <nav
+          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+          id="nav"
+        >
+          <div className="flex flex-row space-x-2">
+            {Object.entries(navItems).map(([path, { name }]) => {
+              return (
+                <Link
+                  key={path}
+                  href={path}
+                  className={cn(
+                    "transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1",
+                    pathname === path ? "border-b" : ""
+                  )}
+                >
+                  {name}
+                </Link>
+              );
+            })}
+          </div>
+        </nav>
+      </div>
+    </aside>
+  );
+}
